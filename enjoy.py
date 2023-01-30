c28d02c20d21b81abec4446a70728f6868d0ecb3
Horrible22232 <sorrybutiwin@web.de>
2023-01-30 16:50:48 +0100

    fixed enjoy

--- enjoy.py
+++ enjoy.py
@@ -5,6 +5,15 @@ from docopt import docopt
 from model import ActorCriticModel
 from utils import create_env
 
+def init_transformer_memory(trxl_conf, model, max_episode_steps, device):
+    memory_mask = torch.tril(torch.ones((trxl_conf["memory_length"], trxl_conf["memory_length"])), diagonal=-1)
+    memory = torch.zeros((1, max_episode_steps, trxl_conf["num_blocks"], trxl_conf["embed_dim"])).to(device)
+    # Setup memory window indices
+    repetitions = torch.repeat_interleave(torch.arange(0, trxl_conf["memory_length"]).unsqueeze(0), max_episode_steps - 1, dim = 0).long()
+    memory_indices = torch.stack([torch.arange(i, i + trxl_conf["memory_length"]) for i in range(max_episode_steps - trxl_conf["memory_length"] + 1)]).long()
+    memory_indices = torch.cat((repetitions, memory_indices))
+    return memory, memory_mask, memory_indices
+
 def main():
     # Command line arguments via docopt
     _USAGE = """
@@ -37,20 +46,23 @@ def main():
     # Run and render episode
     done = False
     episode_rewards = []
-    memory_mask = torch.tril(torch.ones((env.max_episode_steps, env.max_episode_steps)))
-    # Shift mask by one to account for the fact that for the first timestep the memory is empty
-    memory_mask = torch.cat((torch.zeros((1, env.max_episode_steps)), memory_mask))[:-1]  
-    memory = torch.zeros((1, env.max_episode_steps, config["episodic_memory"]["num_layers"], config["episodic_memory"]["layer_size"]), dtype=torch.float32)
-    current_memory_item = torch.zeros((1, 1, config["episodic_memory"]["num_layers"], config["episodic_memory"]["layer_size"]), dtype=torch.float32)
+    memory, memory_mask, memory_indices = init_transformer_memory(config["transformer"], model, env.max_episode_steps, device)
+    memory_length = config["transformer"]["memory_length"]
     t = 0
 
     obs = env.reset()
     while not done:
+        # Prepare observation and memory
+        obs = torch.tensor(np.expand_dims(obs, 0), dtype=torch.float32, device=device)
+        in_memory = memory[0, memory_indices[t].unsqueeze(0)]
+        t_ = max(0, min(t, memory_length - 1))
+        mask = memory_mask[t_].unsqueeze(0)
+        indices = memory_indices[t].unsqueeze(0)
         # Render environment
         env.render()
         # Forward model
-        policy, value, current_memory_item = model(torch.tensor(np.expand_dims(obs, 0), dtype=torch.float32), memory, memory_mask[t].unsqueeze(0))
-        memory[:, t] = current_memory_item
+        policy, value, new_memory = model(obs, in_memory, mask, indices)
+        memory[:, t] = new_memory
         # Sample action
         action = policy.sample().cpu().numpy()
         # Step environemnt
